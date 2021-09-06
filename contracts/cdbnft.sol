e8d053d440b4696bdb6c1f3b69767552c03dfed5
Christof <cdebaes@gmail.com>
2021-09-06 15:17:26 +0200

    feat(deploy): Deploy the chain
    fix(nft): Fix the solidity versioning

--- contracts/cdbnft.sol
+++ contracts/cdbnft.sol
@@ -3,10 +3,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract cdbnft is ERC721, Ownable {
+contract cdbnft is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
