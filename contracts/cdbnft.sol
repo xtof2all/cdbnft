e067037d84d6a0faee00dc31ee4ed93ca3bfe95b
Christof <cdebaes@gmail.com>
2021-09-07 11:43:02 +0200

    chore: Upgrade of the packages

--- contracts/cdbnft.sol
+++ contracts/cdbnft.sol
@@ -3,15 +3,15 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract cdbnft is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("cdbnft", "CDBNFT") {}
+    constructor() ERC721("cdbnft", "CDBNFT") {}
 
     function mintNFT(address recipient, string memory tokenURI)
         public onlyOwner
@@ -26,6 +26,4 @@ contract cdbnft is ERC721URIStorage, Ownable {
         return newItemId;
     }
 }
-
-
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,10 @@
 async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
   const MyNFT = await ethers.getContractFactory("cdbnft")
 
   // Start deployment, returning a promise that resolves to a contract object
@@ -12,4 +18,3 @@ main()
     console.error(error)
     process.exit(1)
   })
-
